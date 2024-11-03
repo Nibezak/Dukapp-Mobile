c7f7d3d30068d199be4c4376fedd78ac9a3f8c1d
Nibezak <kevin.nibeza@gmail.com>
2024-11-03 13:07:04 +0300

    fixed the QR code payment problem on android

--- src/components/QrCode.js
+++ src/components/QrCode.js
@@ -1,31 +1,21 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import { QrCodeSvg, circleRenderer } from 'react-native-qr-svg';
 
 const SIZE = 300;
 
 export default function QrCode({ phoneNumber, orderTotal }) {
-    const CONTENT = `tel:*182*1*1*${phoneNumber}*${orderTotal}#`; // Use both phone number and order total
+    const CONTENT = `tel://${encodeURIComponent(`*182*1*1*${phoneNumber}*${orderTotal}#`)}`;
 
     return (
         <View style={styles.root}>
             <View style={styles.content}>
-                {/* <QrCodeSvg
-                    style={styles.qr}
-                    value={CONTENT}
-                    frameSize={SIZE}
-                    contentCells={5}
-                    content={<Image
-                        source={require('../../assets/dukapp-color.png')}
-                        style={{ width: 45, height: 45, paddingVertical: 10 }}
-                    />}
-                    contentStyle={styles.box}
-                /> */}
                 <QrCodeSvg
                     style={styles.qr}
                     renderer={circleRenderer}
                     value={CONTENT}
                     frameSize={SIZE}
+                    dotColor="#059669"
                 />
             </View>
         </View>
@@ -40,18 +30,11 @@ const styles = StyleSheet.create({
     },
     content: {
         flexDirection: 'row',
-        flexWrap: 'wrap',
-        alignItems: 'center',
-        justifyContent: 'center',
     },
-    box: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    icon: {
-        fontSize: 20,
-    },
-    qr: {
-        padding: 15,
+    payText: {
+        marginTop: 10,
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#059669',
     },
 });
